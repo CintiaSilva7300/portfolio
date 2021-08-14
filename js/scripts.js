344b3698323270c7ffec31b80545fb6d92e14345
Cintia <cintiasilva7300@gmail.com>
2021-08-14 13:57:09 -0300

    ajustes no layout

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,13 @@
 
 function openNav() {
 console.log("alooouuu")
-	var x = document.getElementById("navigation");
+	var x = document.getElementById("navigation"); //pegando o elemento com o id navigation
 
-	if (x.className === "navigation") {
-		x.className += " menujs";
-		document.getElementById("threeline-icon").innerHTML = "&Cross;"
-     }else {
-     	x.className = "navigation"
-     	document.getElementById("threeline-icon").innerHTML = "&#9776;" 
+	if (x.className === "navigation") { //se a classe for igual a navigation
+		x.className += " menujs"; //adiciona a classe menujs
+		document.getElementById("threeline-icon").innerHTML = "&Cross;" //adiciona o icone cross
+     }else { //senao
+     	x.className = "navigation" //adiciona a classe navigation
+     	document.getElementById("threeline-icon").innerHTML = "&#9776;"  //adiciona o icone #9776
      }
 }
